0a2b76de797fb0288ef7a9ada86c968b29dc06ba
Kamil-IKL <161152970+Kamil-IKL@users.noreply.github.com>
2024-03-03 18:45:21 +0500

    Add files via upload

---
+++ 02_ikl_M02_DZ09.py
@@ -0,0 +1,7 @@
+car_list = ['BMW','UAZ','Ford','Lada','Citren']
+cars_count = int()
+for i in car_list:
+    print(f'Я езжу на автомабиле марки: {i} cars_count = {cars_count}')
+    cars_count +=10
+
+
