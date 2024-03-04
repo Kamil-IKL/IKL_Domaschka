76498a06ddae5e470a8ee50fedd90885d9a26b7c
Kamil-IKL <161152970+Kamil-IKL@users.noreply.github.com>
2024-03-04 14:56:03 +0500

    Delete 02_ikl_M02_DZ09.py

--- 02_ikl_M02_DZ09.py
+++
@@ -1,7 +0,0 @@
-car_list = ['BMW','UAZ','Ford','Lada','Citren']
-cars_count = int()
-for i in car_list:
-    print(f'Я езжу на автомабиле марки: {i} cars_count = {cars_count}')
-    cars_count +=10
-
-
