0112d099607f03b05e252419e96391d78b2d6338
Kamil-IKL <161152970+Kamil-IKL@users.noreply.github.com>
2024-03-04 15:35:28 +0500

    Update 02_ikl_M02_DZ10.py

--- 02_ikl_M02_DZ10.py
+++ 02_ikl_M02_DZ10.py
@@ -1,5 +1,5 @@
 car_list = ['BMW', 'UAZ', 'Ford', 'Lada', 'Citren']
-cars_count = int()
+cars_count = int() # int - переводит в целое число ... если (10,25),то --> 10; если (10,52), то --> 11 (учти!) 
 for i in car_list:
     print(f'Я езжу на автомабиле марки: {i} cars_count = {cars_count}')
     cars_count += 10
