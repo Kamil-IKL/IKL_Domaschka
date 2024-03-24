330b682ee17e081e9bf2f1961a16a17604bd686a
Kamil-IKL <161152970+Kamil-IKL@users.noreply.github.com>
2024-03-24 15:07:16 +0500

    Add files via upload

---
+++ 04_ikl_M04_DZ15_main.py
@@ -0,0 +1,5 @@
+import ikl_M04_DZ15
+
+print('\r')
+ikl_M04_DZ15.test_01()
+ikl_M04_DZ15.test_02("fabrizio")
---
+++ ikl_M04_DZ15.py
@@ -0,0 +1,8 @@
+def test_01():
+    print('Hello, world')
+
+def test_02(name):
+    print(f'Hello, world and {name}')
+
+test_01()
+test_02('Muhtar')
