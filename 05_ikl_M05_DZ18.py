d9317f34d2e22a9c11a8f3bb3e877bb3bbf62d6d
Kamil-IKL <161152970+Kamil-IKL@users.noreply.github.com>
2024-04-09 18:00:08 +0500

    Update 05_ikl_M05_DZ18.py

--- 05_ikl_M05_DZ18.py
+++ 05_ikl_M05_DZ18.py
@@ -1,17 +1,18 @@
 class House:
     def __init__(self, NumberOfFloors):
         self.NumberOfFloors = NumberOfFloors
+
     def setNewNumberOfFloors(self):
         floors = self.NumberOfFloors
         print(f'Вывожу на консоль NumberOfFloors = {floors}')
 
 
-a1 = House(10) # создаю новый объект с атрибутом 10
-a2 = House(15) # создаю новый объект с атрибутом 15
-a3 = House(25) # создаю новый объект с атрибутом 25
-a1.setNewNumberOfFloors() # вызываю метод(функцию) в объекте с атрибутом 10
-a2.setNewNumberOfFloors() # вызываю метод(функцию) в объекте с атрибутом 15
-a3.setNewNumberOfFloors() # вызываю метод(функцию) в объекте с атрибутом 25
+a1 = House(10)  # создаю новый объект с атрибутом 10
+a2 = House(15)  # создаю новый объект с атрибутом 15
+a3 = House(25)  # создаю новый объект с атрибутом 25
+a1.setNewNumberOfFloors()  # вызываю метод(функцию) в объекте с атрибутом 10
+a2.setNewNumberOfFloors()  # вызываю метод(функцию) в объекте с атрибутом 15
+a3.setNewNumberOfFloors()  # вызываю метод(функцию) в объекте с атрибутом 25
 
 
 
