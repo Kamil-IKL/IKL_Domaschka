ba73a4b4ce63d6bc94cef4ac4c29b87e0c1d5b48
Kamil-IKL <161152970+Kamil-IKL@users.noreply.github.com>
2024-04-24 10:29:41 +0500

    Add files via upload

---
+++ 05_ikl_M05_DZ20.py
@@ -0,0 +1,21 @@
+class Buiding:  # создал класс строительство
+    total = 0  # создал атрибут класса
+
+    def __init__(self):  # создал инициализатор для класса
+        Building = Buiding.total + 1
+
+
+
+# Building_1 = Buiding()
+# Building_2 = Buiding()
+# Building_3 = Buiding()
+# print(Building_1, type(Building_1), id(Building_1))
+# print(Building_2, type(Building_2), id(Building_2))
+# print(Building_3, type(Building_3), id(Building_3))
+
+
+for i in range(40):
+    Building = Buiding()
+    print(Building, type(Building), id(Building))
+
+
