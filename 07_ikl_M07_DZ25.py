82ab0e49fbe6127553de52960ad1c4b65eb3c35a
Kamil-IKL <161152970+Kamil-IKL@users.noreply.github.com>
2024-04-26 20:38:56 +0500

    Update 07_ikl_M07_DZ25.py

--- 07_ikl_M07_DZ25.py
+++ 07_ikl_M07_DZ25.py
@@ -7,7 +7,7 @@ for root, dirs, files in os.walk(directory):
     for file in files:
         filepath = os.path.join(root, file) # полный путь к файлу
         filetime = os.path.getmtime(filepath) # время создания файла
-        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime)) # перефожу в формат ДД.ММ.ГГГГ
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime)) # перевожу в формат ДД.ММ.ГГГГ
         filesize = os.path.getsize(filepath) # определяю размер файла
         parent_dir = os.path.dirname(filepath) # определяю в какой папке находится файл
         print(f'\n {'*' * 30}')
