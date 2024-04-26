6016d0028598e5b99e6bf21c8efdb00c200bef97
Kamil-IKL <161152970+Kamil-IKL@users.noreply.github.com>
2024-04-26 19:09:27 +0500

    Add files via upload

---
+++ 07_ikl_M07_DZ25.py
@@ -0,0 +1,18 @@
+import os
+import time
+
+directory = r'proekt_DZ25'
+
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        filepath = os.path.join(root, file) # полный путь к файлу
+        filetime = os.path.getmtime(filepath) # время создания файла
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime)) # перефожу в формат ДД.ММ.ГГГГ
+        filesize = os.path.getsize(filepath) # определяю размер файла
+        parent_dir = os.path.dirname(filepath) # определяю в какой папке находится файл
+        print(f'\n {'*' * 30}')
+        print(f'Обнаружен файл: {file} \nПуть: {filepath} \nРазмер: {filesize} '
+              f'байт. Время изменения: {formatted_time} \nРодительская директория: {parent_dir}')
+        print(f'\n {'*' * 30}')
+
+
