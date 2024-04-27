c999bf2a7a4bffa4aa998eed6722852739546b32
Kamil-IKL <161152970+Kamil-IKL@users.noreply.github.com>
2024-04-27 17:04:32 +0500

    Update 07_ikl_M07_DZ25.py

--- 07_ikl_M07_DZ25.py
+++ 07_ikl_M07_DZ25.py
@@ -10,9 +10,9 @@ for root, dirs, files in os.walk(directory):
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime)) # перевожу в формат ДД.ММ.ГГГГ
         filesize = os.path.getsize(filepath) # определяю размер файла
         parent_dir = os.path.dirname(filepath) # определяю в какой папке находится файл
-        print(f'\n {'*' * 30}')
+        print(f'\n {'*' * 50}')
         print(f'Обнаружен файл: {file} \nПуть: {filepath} \nРазмер: {filesize} '
               f'байт. Время изменения: {formatted_time} \nРодительская директория: {parent_dir}')
-        print(f'\n {'*' * 30}')
+        print(f'\n {'*' * 50}')
 
 
