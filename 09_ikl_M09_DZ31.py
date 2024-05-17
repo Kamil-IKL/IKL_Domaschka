42c03440bc8ccdf62a70dcf0fe31667f4f8188e6
Kamil-IKL <161152970+Kamil-IKL@users.noreply.github.com>
2024-05-17 14:23:35 +0500

    Update 09_ikl_M09_DZ31.py

--- 09_ikl_M09_DZ31.py
+++ 09_ikl_M09_DZ31.py
@@ -3,24 +3,26 @@
 # start – начальное значение (если значение не передано, то 0)
 # end – конечное значение (если значение не передано, то 1)
 class EvenNumbers:
-    def __iter__(self):
-        return self
-
     def __init__(self, start, end):
         self.start = start
         self.end = end
-        self.count = start  # это счетчик
+        self.count = start  # это для счетчика
+
+    def __iter__(self):  # Без этого метода итератора объект неитерируемый (for работать не будет!!!)
+        return self
 
     def __next__(self):
         if self.count < self.end:
-            self.count += 1
-            if self.count % 2 == 0:  # сделал условие для четных чисел
+            self.count += 1  # это счетчик
+            if self.count % 2 == 0:  # сделал условие для четного числа
+                return self.count  # возвращаю текущее число
+            else:
+                self.count += 1
                 return self.count
         else:
             raise StopIteration
 
 
 en = EvenNumbers(10, 25)
-
 for i in en:
     print(i)
