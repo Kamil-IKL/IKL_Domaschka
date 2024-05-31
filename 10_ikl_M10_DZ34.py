e00c3b9e63adb79e468d36514728f1b1e00c71de
Kamil-IKL <161152970+Kamil-IKL@users.noreply.github.com>
2024-05-31 13:45:25 +0500

    Update 10_ikl_M10_DZ34.py

--- 10_ikl_M10_DZ34.py
+++ 10_ikl_M10_DZ34.py
@@ -7,21 +7,24 @@
 import time
 import threading
 
+
 def my_funct_num():
-    for i in range(1,11):
+    for i in range(1, 11):
         time.sleep(1)
         print(i)
 
+
 def my_funct_alphabet():
-    for i in range(97,107): # начиная с 97 - это алфавит, если использовать "chr"
+    for i in range(97, 107):  # начиная с 97 - это алфавит, если использовать "chr"
         print(chr(i))
         time.sleep(1)
     # alphabet = [print(chr(i)) for i in range(97, 107)] # тоже самое - это запись в строку, но не знаю
-                                                         # как сделать интервал по времени (куда воткнуть "time.sleep")
+    # как сделать интервал по времени (куда воткнуть "time.sleep()")
 
-thr_num = threading.Thread(target=my_funct_num) # создаю поток по числам (см. задание)
-thr_alp = threading.Thread(target=my_funct_alphabet) # создаю поток по буквам (см. задание)
-thr_num.start() # запускаю поток по числам (см. задание)
+
+thr_num = threading.Thread(target=my_funct_num)  # создаю поток по числам (см. задание)
+thr_alp = threading.Thread(target=my_funct_alphabet)  # создаю поток по буквам (см. задание)
+thr_num.start()  # запускаю поток по числам (см. задание)
 # print(threading.enumerate()) # проверяю сколько "живых" потоков на данный момент запущено
-thr_alp.start() # запускаю поток по буквам (см. задание)
-print(threading.enumerate()) # проверяю сколько "живых" потоков на данный момент запущено (обращаю внимание - работает)
+thr_alp.start()  # запускаю поток по буквам (см. задание)
+print(threading.enumerate())  # проверяю сколько "живых" потоков на данный момент запущено (обращаю внимание - работает)
