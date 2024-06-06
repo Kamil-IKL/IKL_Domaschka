48371da74862ef3537578028b3fa429d85628244
Kamil-IKL <161152970+Kamil-IKL@users.noreply.github.com>
2024-06-06 15:59:02 +0500

    Update 10_ikl_M10_DZ36.py

--- 10_ikl_M10_DZ36.py
+++ 10_ikl_M10_DZ36.py
@@ -5,22 +5,72 @@
 # модификации общего ресурса.
 
 import threading
+import time
 
-class BankAccount(threading.Thread):
-    def __init__(self):
+lock = threading.Lock()  # создал замок
+
+
+# создал генератор номера счета
+def account_number():
+    import random
+    account = ''
+    for i in range(5):
+        account = account + random.choice(list('0123456789'))
+    return account
+
+
+account = account_number() # создаю номер счета Клиента
+balance = int(input('Внесите первый взнос на счет: '))  # создал баланс счета Клиента через запрос
+print(f'{"":*^50}')
+print(f'Баланс Вашего счета №"{account} составляет {balance}')
+print(f'{"":*^50}')
+
+
+class BankAccount(threading.Thread): # создал класс движения ДС на счете Клиента
+    def __init__(self, amount_incoming, amount_spending):
         super().__init__()  # метод вызова всех функций из класса родителя "Thread"
-        account_balance = int(input('Внесите первый взнос на счет: ')) # баланс счета клиента
+        self.amount_incoming = amount_incoming  # сумма пополнения счета
+        self.amount_spending = amount_spending  # сумма списания счета
+        global account, balance # перенес сюда глобальные переменные (номер и баланс счета Клиента)
 
-    def Bank_accoint(self,incoming_money, spending_money): # создаю банковский счет клиента
-        self.incoming_money = incoming_money # приход ДС на счет клиента
-        self.spending_money = spending_money # расход ДС со счета клиента
+    def incoming_money(self):  # пополнение счета Клиента
+        a = balance # остаток на счете Клиента
+        with lock: # включил локальный замок с автозакрытием
+            for i in range(5): # логика пополнения счета Клиента (5 итераций)
+                time.sleep(1)
+                a += self.amount_incoming
+                print(f'Сумма пополнения = {self.amount_incoming},  баланс Вашего счета №{account} = {a}')
+            return a
 
-    def incoming_money(self,amount_incoming): # пополнение счета клиента
-        self.amount_incoming = amount_incoming # сумма пополнения
+    def spending_money(self):  # списание со счета Клиента
+        b = balance # остаток на счете Клиента
+        with lock:  # включил локальный замок с автозакрытием
+            for j in range(5): # логика списания со счета Клиента (5 итераций)
+                time.sleep(1)
+                b += -self.amount_spending
+                print(f'Сумма списания = {-self.amount_spending}, баланс Вашего счета №{account} = {b}')
+            return b
 
-        pass
+    def run(self):  # переопределил метод run
+        a = self.amount_incoming * 5  # т.к. 5 итераций в цикле функции incoming_money(self)
+        b = self.amount_spending * 5  # т.к. 5 итераций в цикле функции spending_money(self)
+        if a > 0 and b == 0:
+            BankAccount.incoming_money(self)
+            # print(f'ПРОВЕРКА: после ВСЕХ пополнений = {a} баланс Вашего счета №{account} = {balance}')
+        elif b > 0 and a == 0:
+            BankAccount.spending_money(self)
+            # print(f'ПРОВЕРКА: после Всех списаний = {b}, баланс Вашего счета №{account} = {balance}')
+        elif a == 0 and b == 0:
+            print(f'Баланс Вашего счета №{account} остался без изменений'.upper())
+        else:
+            print('Один из аргументов класса'.upper() + ' BankAccount() ' + 'должен быть равен 0'.upper())
 
-    def spending_money(self): # списание со счета клиента
-        pass
 
-
+incoming = BankAccount(10, 0) # создаю поток пополнение счета Клиента
+spending = BankAccount(0, 5) # создаю поток списания со счета Клиента
+incoming.start() # запуск потока пополнения счета Клиента
+spending.start() # запуск потока счписания со счета Клиента
+incoming.join() # честно говоря, так и не понял, для чего этот метод нужен ?
+spending.join() # честно говоря, так и не понял, для чего этот метод нужен ?
+# time.sleep(11)
+print(f'ПРОВЕРКА: после ВСЕХ операций баланс Вашего счета №{account} = {balance}')
