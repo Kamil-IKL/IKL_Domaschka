7ddf35f53054adce30ce3defaa0551ed88bd8af7
Kamil-IKL <161152970+Kamil-IKL@users.noreply.github.com>
2024-08-09 09:56:42 +0500

    Add files via upload

---
+++ 12_ikl_M12_DZ_42_unittest.py
@@ -0,0 +1,31 @@
+import runner
+import unittest
+
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        obj_1 = runner.Runner("VALERA")
+        for i in range(10):
+            obj_1.walk()
+            # print(obj_1.distance) # для проверки результата
+        self.assertEqual(obj_1.distance, 50)
+
+    def test_run(self):
+        obj_2 = runner.Runner('Leonid')
+        for i in range(10):
+            obj_2.run()
+            # print(obj_2.distance) # для проверки результата
+        self.assertEqual(obj_2.distance, 1000)
+
+    def test_challenge(self):
+        obj_1 = runner.Runner("VALERA")
+        obj_2 = runner.Runner('Leonid')
+        for i in range(10):
+            obj_1.walk()
+            obj_2.run()
+            # print(obj_1.distance, obj_2.distance)  # для проверки результата
+        self.assertNotEqual(obj_1.distance, obj_2.distance)
+
+
+if __name__ == '__main__':
+    unittest.main()
